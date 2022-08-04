e78bbefdf523217645a29031cac54a94d26ae7f5
oseunghyun <ohsh1477@gmail.com>
2022-08-04 23:46:07 +0900

    [feat] Users api 함수 구현

---
+++ src/api/user.js
@@ -0,0 +1,30 @@
+import { instance } from "./index";
+
+// 프로필 수정 
+function editProfile(userData) {
+  return instance.post("users/edit", userData);
+}
+
+// 비밀번호 재설정 
+function resetPw(password) {
+  return instance.post("users/reset-password", password);
+}
+
+// 프로필 조회 
+function fetchProfile() {
+  return instance.get("users/me");
+}
+
+// 자신의 아티클 조회 
+function fetchMyArticle(categoryId) {
+  return instance.get(`users/load-contents?categoryId=${categoryId}`);
+}
+
+// 자신의 카테고리 목록 조회
+function fetchMyCategory() {
+  return instance.get("users/load-categories");
+}
+
+
+
+export { editProfile, resetPw, fetchProfile, fetchMyArticle, fetchMyCategory };
