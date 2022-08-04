28da61f0d973a20223e46eeaf86fbe6f09f5b700
oseunghyun <ohsh1477@gmail.com>
2022-08-04 23:57:02 +0900

    [feat] Auth api 함수 구현

---
+++ src/api/auth.js
@@ -0,0 +1,39 @@
+import { instance } from "./index";
+
+// 회원가입
+function registerUser(userData) {
+  return instance.post("auth/register", userData);
+}
+
+// 로그인
+function loginUser(userData) {
+  return instance.post("auth/login", userData);
+}
+
+// 로그아웃
+function logoutUser() {
+  return instance.get("auth/logout");
+}
+
+// 회원탈퇴
+function deleteUser() {
+  return instance.delete("auth/delete");
+}
+
+// 토큰 재발행
+function reissueToken(refreshToken) {
+  return instance.post("auth/reissue", refreshToken);
+}
+
+// 비밀번호 재설정을 위한 메일 전송
+function resetPw(email) {
+  return instance.get(`auth/send-password-reset-email/${email}`);
+}
+
+// 이메일 인증
+function verifyEmail(code) {
+  return instance.get(`auth/verify-email?code=${code}`);
+}
+
+
+export { registerUser, loginUser, logoutUser, deleteUser, reissueToken, resetPw, verifyEmail };
