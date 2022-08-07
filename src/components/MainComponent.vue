0d4522af78e134acd6d657164eaac96dd233a890
oseunghyun <ohsh1477@gmail.com>
2022-08-07 22:29:45 +0900

    [feat] 로그인 기능 구현

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>메인</h1>
+    <span v-if="isUserLogin">{{ $store.state.userEmail }}님 환영합니다.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    isUserLogin() {
+      return this.$store.getters.isLogin;
+    },
+  }
+}
+</script>
+
+<style>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,48 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import {
+  getAuthFromCookie,
+  getUserFromCookie,
+  saveAuthToCookie,
+  saveUserToCookie
+} from "@/utils/cookies";
+import { loginUser } from "@/api/auth";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-      
+      userEmail: getUserFromCookie() || "",
+      accessToken: getAuthFromCookie() || "",
+      refreshToken: localStorage.getItem("refreshToken") || ""
   },
   getters: {
+    isLogin(state) {
+      return state.userEmail !== "";
+    },
   },
   mutations: {
+    setAccessToken(state, token) {
+      state.accessToken = token;
+    },
+    setRefreshToken(state, token) {
+      state.refreshToken = token;
+    },
+    setUserEmail(state, userEmail) {
+      state.userEmail = userEmail;
+    },
   },
   actions: {
-    
+    async LOGIN({ commit }, userData) {
+      const { data } = await loginUser(userData);
+      console.log(`${userData.email}님 로그인 완료`);
+      commit("setAccessToken", data.access_token);
+      commit("setRefreshToken", data.refresh_token);
+      commit("setUserEmail", userData.email);
+      saveUserToCookie(userData.email);
+      saveAuthToCookie(data.access_token);
+      localStorage.setItem("refreshToken", data.refresh_token);
+      return data;
+    },
   }
 });
