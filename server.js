7dd9611af1e65176899ea886a38a0f9345e4d722
oseunghyun <ohsh1477@gmail.com>
2022-08-07 13:19:20 +0900

    Fix router

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const serverStatic = require('serve-static')
 const path = require('path')
+const history = require('connect-history-api-fallback');
+
 
 const app = express()
 const port = process.env.PORT || 3000
+app.use(history());
+
 
 // SERVES FILES from our dist directory which now contains out index.html
 app.use('/', serverStatic(path.join(__dirname, '/dist')))
@@ -11,3 +15,6 @@ app.listen(port)
 console.log('Listening on port:' + port)
 // app.get('/', (req, res) => res.send('Hello World!'))
 // app.listen(port, () => console.log(`Example app listening on port port!`))
+
+
+
