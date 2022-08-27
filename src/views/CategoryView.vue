a6c29b070f3a6f28ffbdb6e35ace81b641ed080c
oseunghyun <ohsh1477@gmail.com>
2022-08-27 19:50:17 +0900

    [feat] 카테고리 - 콜렉션 컴포넌트 UI 구현

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -3,6 +3,9 @@
     <h1 class="page-header">{{ category.name }}<img :src="setting" /></h1>
     <div>
       <contents-component :contentsData="contentsData"></contents-component>
+      <collection-component
+        :collectionData="collectionData"
+      ></collection-component>
     </div>
   </div>
 </template>
@@ -10,16 +13,18 @@
 <script>
 import setting from "@/assets/icon/settings.svg";
 import ContentsComponent from "@/components/contents/ContentsComponent.vue";
+import CollectionComponent from "../components/contents/CollectionComponent.vue";
 
 export default {
-  components: { ContentsComponent },
+  components: { ContentsComponent, CollectionComponent },
   data() {
     return {
       setting,
+      // 더미 데이터
       category: {
         name: "카테고리명",
       },
-      // 더미 데이터
+      // 콘텐츠 더미 데이터
       contentsData: {
         title: "까일 수 밖에 없는 기획안 VS 까이지 않는 기획안",
         contents:
@@ -28,6 +33,18 @@ export default {
         date: "2022.08.20",
         expiryDate: "3",
         favorites: false,
+      },
+      // 콜렉션 더미 데이터
+      collectionData: {
+        title: "비즈니스 모델 분석법",
+        lists: [
+          "10년차 컨설턴트의 게스티네이션 노하우",
+          "KSF모델로 보는 경쟁사 분석",
+          "수익모델 분석방법",
+          "AARRR분석하기 기초",
+          "분석하기 고급",
+        ],
+        favorites: true,
       },
     };
   },
