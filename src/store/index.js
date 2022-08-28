a9d0de6078e48b30e6ff90359014f830273fb59a
oseunghyun <ohsh1477@gmail.com>
2022-08-28 16:11:16 +0900

    [refactor] 로그인 여부 및 프로필 조회를 vuex에서 관리하도록 수정

--- src/store/index.js
+++ src/store/index.js
@@ -9,14 +9,15 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     nickname: "",
+    loginState: false,
     accessToken: getAuthFromCookie(),
     refreshToken: localStorage.getItem("refreshToken"),
     oauthInfo: localStorage.getItem("oauthInfo"),
   },
   getters: {
+    // 로그인 여부 확인
     isLogin(state) {
-      // 토큰 여부로 수정하기
-      return state.nickname !== "";
+      return state.loginState == true;
     },
     // 소셜 로그인 여부
     isOauthLogin(state) {
@@ -36,8 +37,12 @@ export default new Vuex.Store({
     logoutUser(state) {
       state.nickname = "";
     },
+    setLoginState(state, loginState) {
+      state.loginState = loginState;
+    },
   },
   actions: {
+    // 로그인
     async LOGIN({ commit }, userData) {
       const { data } = await loginUser(userData);
       commit("setRefreshToken", data.refresh_token);
@@ -46,6 +51,22 @@ export default new Vuex.Store({
       const response = await fetchProfile();
       commit("setNickname", response.data.name);
       return data;
+    },
+    // 프로필 조회이자 로그인 여부 확인
+    async FETCH_PROFILE(commit) {
+      try {
+        const { response } = await fetchProfile();
+        commit("setNickname", response.data.name);
+        console.log("여긴 vuex, 로그인 여부 확인중", response);
+        if (response.data.statusCode == 200) {
+          commit("setLoginState", true);
+        }
+      } catch (error) {
+        console.log(error);
+        if (error.response.data.statusCode == 401) {
+          commit("setLoginState", false);
+        }
+      }
     },
   },
 });
