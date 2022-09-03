173f16dbc0d85eb99d8660413174ce434bf92b9c
oseunghyun <ohsh1477@gmail.com>
2022-09-03 13:56:47 +0900

    [feat] 즐겨찾기 카테고리 상세 페이지 구현

--- src/components/category/FavoriteCategoryComponent.vue
+++ src/components/category/FavoriteCategoryComponent.vue
@@ -14,151 +14,67 @@
         :key="index"
         :contentsData="contents"
       ></contents-component>
-      <collection-component
-        v-show="isCollectionActive"
-        :collectionData="collectionData"
-      ></collection-component>
     </div>
-    <!-- 카테고리 수정 모달 컴포넌트 -->
-    <category-modal-component
-      v-show="isCategoryModalActive"
-      @close-modal="isCategoryModalActive = false"
-      :categoryModalTitle="categoryModalTitle"
-      @categoryEvent="categoryEvent"
-      :categoryName="newCategoryName"
-      @deleteCategory="deleteCategory"
-      :deleteBtn="deleteBtn"
-    ></category-modal-component>
   </div>
 </template>
 
 <script>
-import setting from "@/assets/icon/settings.svg";
 import ContentsComponent from "@/components/contents/ContentsComponent.vue";
-import CollectionComponent from "@/components/contents/CollectionComponent.vue";
-import CategoryModalComponent from "@/components/modal/CategoryModalComponent.vue";
-import { fetchMyContents, fetchMyCollections } from "@/api/user";
-import { updateCategory, deleteCategory } from "@/api/category";
-import { fetchMyCategory } from "@/api/user";
+import { fetchMyFavorites } from "@/api/user";
 
 export default {
   components: {
     ContentsComponent,
-    CollectionComponent,
-    CategoryModalComponent,
   },
   data() {
     return {
       isCollectionActive: false,
       isCategoryModalActive: false,
-      categoryModalTitle: "카테고리 수정",
       categoryFilter: "latest",
-      setting,
       categoryId: "",
-      categoryName: "",
-      newCategoryName: "",
-      deleteBtn: "카테고리 삭제",
-      // 콘텐츠 props 데이터
-      contentsData: {},
-      // 콜렉션 더미 데이터
-      collectionData: {
-        title: "비즈니스 모델 분석법",
-        lists: [
-          "10년차 컨설턴트의 게스티네이션 노하우",
-          "KSF모델로 보는 경쟁사 분석",
-          "수익모델 분석방법",
-          "AARRR분석하기 기초",
-          "분석하기 고급",
-        ],
-        favorites: true,
-      },
-      path: this.$route.params.id,
+      // 즐겨찾기 더미 데이터
+      contentsData: [
+        {
+          title:
+            "제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목ㄴ",
+          createdAt: "2022-09-09",
+          link: "http://naver.com",
+          favorite: true,
+          comment: "메모",
+          deadline: "2022-09-28",
+          description: "설명",
+        },
+        {
+          title: "제목",
+          createdAt: "2022-09-09",
+          link: "http://naver.com",
+          favorite: true,
+          comment: "메모",
+          deadline: "2022-09-28",
+          description: "설명",
+        },
+        {
+          title: "제목",
+          createdAt: "2022-09-09",
+          link: "http://naver.com",
+          favorite: true,
+          comment: "메모",
+          description: "설명",
+        },
+      ],
     };
   },
   async created() {
-    this.categoryId = this.$route.params.id;
-    this.fetchContentsList();
-    this.fetchCategoryName();
-  },
-  watch: {
-    $route() {
-      this.fetchContentsList();
-      this.fetchCategoryName();
-      this.categoryId = this.$route.params.id;
-    },
+    this.fetchFavoritesList();
   },
   methods: {
     // 나의 콘텐츠 조회
-    async fetchContentsList() {
-      const categoryId = this.$route.params.id;
-      console.log("카테고리 id", categoryId);
+    async fetchFavoritesList() {
       try {
-        const response = await fetchMyContents(categoryId);
+        const response = await fetchMyFavorites();
         // 콘텐츠 컴포넌트에 데이터 전달
-        this.contentsData = response.data.contents;
+        this.contentsData = response.data.favorites;
         console.log("콘텐츠 데이터", this.contentsData);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    // 나의 콜렉션 조회
-    async fetchCollectionList() {
-      this.categoryId = this.$route.params.id;
-      console.log("카테고리 id", this.categoryId);
-      try {
-        const response = await fetchMyCollections(this.categoryId);
-        // 콘텐츠 컴포넌트에 데이터 전달
-        this.collectionData = response.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    // 카테고리 추가 모달 열기
-    openCategoryModal() {
-      this.isCategoryModalActive = true;
-    },
-    // 카테고리 수정
-    async categoryEvent(newCategoryName) {
-      this.newCategoryName = newCategoryName;
-      try {
-        const categoryData = {
-          name: this.newCategoryName,
-          originalName: this.categoryName,
-        };
-        const response = await updateCategory(categoryData);
-        console.log(response);
-        this.categoryName = this.newCategoryName;
-        this.isCategoryModalActive = false;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    // 카테고리 삭제
-    async deleteCategory() {
-      try {
-        const response = await deleteCategory(this.categoryId);
-        console.log(response);
-        this.isCategoryModalActive = false;
-        this.$router.push("/category/all");
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    // 카테고리 이름 조회
-    async fetchCategoryName() {
-      try {
-        const response = await fetchMyCategory();
-        const categoryId = this.$route.params.id;
-        const categories = response.data.categories;
-        if (categoryId == -1) {
-          this.categoryName = "미분류";
-        } else {
-          const categoryFilter = categories.filter(function (cate) {
-            return cate.id == categoryId;
-          });
-          console.log(categoryFilter);
-          this.categoryName = categoryFilter[0].name;
-        }
       } catch (error) {
         console.log(error);
       }
