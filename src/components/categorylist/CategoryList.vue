1a4b2021489ad40f576b28f48019ae62d8427289
oseunghyun <ohsh1477@gmail.com>
2022-09-06 23:53:38 +0900

    [rename] 카테고리 목록 컴포넌트들 CategoryList-로 이름 변경

---
+++ src/components/categorylist/CategoryList.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="all-content">
+    <header>
+      <div @click="toCategoryPage()" class="content__header">
+        <h3>{{ categoryTitle }}</h3>
+      </div>
+      <button @click="showContent()">
+        <img v-if="!contentState" :src="plus" />
+        <img v-if="contentState" :src="minus" />
+      </button>
+    </header>
+    <!-- 콘텐츠 목록 -->
+    <div v-if="contentState" class="contents-lists">
+      <div
+        v-for="(content, index) in contentsList"
+        :key="index"
+        class="contents-list"
+      >
+        <div class="contents-list__wrapper">
+          <button class="btn--transparent--img" @click="toLink(content.link)">
+            <span class="contents-list__icon">웹</span>
+            <span class="contents-list__title">
+              {{ filterTitle(content.title) }}
+            </span>
+          </button>
+        </div>
+        <div class="contents-list__wrapper">
+          <img :src="line" />
+          <span v-if="content.deadline" class="contents-list__expiry"
+            >D-{{ countDday(content.deadline) }}</span
+          >
+          <button class="btn--transparent" @click="openMemoModal(index)">
+            <img :src="memo" />
+          </button>
+          <button class="btn--transparent" @click="createFavorites(index)">
+            <img v-if="content.favorite" :src="star" />
+            <img v-if="!content.favorite" :src="star_gray" />
+          </button>
+        </div>
+      </div>
+    </div>
+    <memo-modal-component
+      v-if="isMemoModalActive"
+      @close-modal="isMemoModalActive = false"
+      :memoContents="memoContents"
+    ></memo-modal-component>
+  </div>
+</template>
+
+<script>
+import plus from "@/assets/icon/plus.svg";
+import minus from "@/assets/icon/minus.svg";
+import line from "@/assets/icon/line.svg";
+import memo from "@/assets/icon/memo.svg";
+import star from "@/assets/icon/star.svg";
+import star_gray from "@/assets/icon/star_gray.svg";
+import { fetchMyContents } from "@/api/user";
+import MemoModalComponent from "@/components/modal/MemoModalComponent.vue";
+import { countDday } from "@/utils/validation";
+import { addFavorite } from "@/api/contents";
+
+export default {
+  components: { MemoModalComponent },
+  data() {
+    return {
+      plus,
+      minus,
+      line,
+      memo,
+      star,
+      star_gray,
+      contentState: false,
+      isMemoModalActive: false,
+      // 더미 데이터
+      contentsList: [
+        {
+          title: "제목",
+          link: "https://naver.com",
+          comment: "메모",
+          categoryName: "카테고리",
+          favorite: true,
+          description: "설명이고",
+        },
+      ],
+    };
+  },
+  props: {
+    categoryTitle: String,
+    categoryId: Number,
+  },
+  created() {
+    this.fetchContentsList();
+  },
+  methods: {
+    showContent() {
+      this.contentState = !this.contentState;
+    },
+    // 즐겨찾기 생성
+    async createFavorites(index) {
+      console.log("인덱스", index);
+      this.contentsList[index].favorite = !this.contentsList[index].favorite;
+      try {
+        const contentId = this.contentsList[index].id;
+        const response = await addFavorite(contentId);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    // 카테고리 상세 페이지로 이동
+    toCategoryPage() {
+      this.$router.push(`/category/${this.categoryId}`);
+    },
+    // 나의 콘텐츠 조회 (카테고리 아이디 받아서 조회하기)
+    async fetchContentsList() {
+      try {
+        const response = await fetchMyContents(this.categoryId);
+        // 콘텐츠 컴포넌트에 데이터 전달
+        this.contentsList = response.data.contents;
+        console.log("콘텐츠 데이터", this.contentsList);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    // 메모 모달 열기
+    openMemoModal(index) {
+      this.isMemoModalActive = true;
+      this.memoContents = this.contentsList[index].comment;
+    },
+    // 제목 글자수 30자 이상
+    filterTitle(title) {
+      if (title.length >= 30) {
+        return title.substr(0, 30) + "...";
+      } else {
+        return title;
+      }
+    },
+    countDday(deadline) {
+      console.log(countDday(deadline));
+      return countDday(deadline);
+    },
+    toLink(link) {
+      window.open(link, "_blank");
+    },
+  },
+};
+</script>
+
+<style></style>
