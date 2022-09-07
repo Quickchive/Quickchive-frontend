0c2a465d25b4179865896be0fbe2aca82c62c3a9
oseunghyun <ohsh1477@gmail.com>
2022-09-07 11:30:17 +0900

    [fix] 정렬 기준 새로 정의된 기준에 맞게 sort.js 내 함수 수정

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,6 +1,7 @@
 /**** 콘텐츠 + 콜렉션 배열 합쳐서 */
 
-// 1. 최신순 정렬(createdAt) - 배열 두 개 합친 후에
+// 1. 최신순 정렬(createdAt)
+// - 콘텐츠 및 콜렉션을 우리 웹사이트에 가장 최근에 저장한 순서대로 노출
 function sortLatestArr(contentsData, collectionData) {
   // 1) 콘텐츠 배열과 콜렉션 배열을 합친다.(spread operator: 전개연산자)
   const newArr = [...contentsData, ...collectionData];
@@ -11,13 +12,30 @@ function sortLatestArr(contentsData, collectionData) {
   return sortedArr;
 }
 
-// 2. 즐겨찾기 순 - 즐겨찾기를 지정한 날짜 순
+// 2. 즐겨찾기 순
+// - 즐겨찾기를 지정한 날짜순
+// *즐겨찾기가 지정된 콘텐츠 및 콜렉션은 수정된 날짜 순서대로 노출
+// (예: 22.8.23 23:34 저장 + 22.9.10에 즐겨찾기 지정 > 22.9.6 14:00 저장 + 22.9.6 14:03에 즐겨찾기 지정)
 function sortFavoritesArr(contentsData, collectionData) {
   const newArr = [...contentsData, ...collectionData];
-  const arr = newArr.sort(
+
+  // 즐겨찾기 있는 것만 분류
+  const favoritesArr = newArr.filter((data) => data.favorite == true);
+  // 즐겨찾기 없는 것만 분류
+  const NonFavoritesArr = newArr.filter((data) => data.favorite == false);
+
+  // 즐겨찾기 된 것들을 최신순으로 정렬
+  const sortedFavorites = favoritesArr.sort(
+    (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
+  );
+  // 즐겨찾기 되지 않은 것들을 최신순으로 정렬
+  const sortedNonFavorites = NonFavoritesArr.sort(
     (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
   );
-  const sortedArr = arr.filter((data) => data.favorites == true);
+
+  // 위 두 배열을 합침
+  const sortedArr = [...sortedFavorites, ...sortedNonFavorites];
+
   return sortedArr;
 }
 
@@ -25,11 +43,27 @@ function sortFavoritesArr(contentsData, collectionData) {
 function sortDeadlineArr(contentsData, collectionData) {
   const newArr = [...contentsData, ...collectionData];
 
-  const arr = newArr.filter((data) => data.deadline);
-
-  const sortedArr = arr.sort(
-    (a, b) => new Date(b.deadline) - new Date(a.deadline)
+  // 기한이 있는 것만 분류
+  const deadlineArr = newArr.filter(
+    (data) => data.deadline == !"" || !undefined || !null
   );
+  // 기한이 없는 것만 분류
+  const NonDeadlineArr = newArr.filter(
+    (data) => data.deadline == "" || undefined || null
+  );
+
+  // 기한이 있는 것들을 최신순으로 정렬
+  const sortedDeadline = deadlineArr.sort(
+    (a, b) => new Date(a.deadline) - new Date(b.deadline)
+  );
+  // 기한이 있는 것들을 최신순으로 정렬
+  const sortedNonDeadline = NonDeadlineArr.sort(
+    (a, b) => new Date(a.deadline) - new Date(b.deadline)
+  );
+
+  // 위 두 배열을 합침
+  const sortedArr = [...sortedDeadline, ...sortedNonDeadline];
+
   return sortedArr;
 }
 
