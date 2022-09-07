ebba15f0f2fe07696bc7806bbaf5d2a34bb3eefc
oseunghyun <ohsh1477@gmail.com>
2022-09-07 20:56:51 +0900

    [feat] 로그인 유지 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
-<div>
-  <the-header></the-header>
-  <router-view></router-view>
-  <the-footer></the-footer>
-</div>
+  <div>
+    <the-header></the-header>
+    <router-view></router-view>
+    <the-footer></the-footer>
+  </div>
 </template>
 
 <script>
-import TheFooter from './components/common/TheFooter.vue'
-import TheHeader from './components/common/TheHeader.vue'
-
+import TheFooter from "./components/common/TheFooter.vue";
+import TheHeader from "./components/common/TheHeader.vue";
+import { deleteCookie } from "@/utils/cookies";
 export default {
   components: { TheHeader, TheFooter },
-  name: 'App',
-}
+  name: "App",
+  data() {
+    return {
+      isLeaveSite: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("beforeunload", this.unLoadEvent);
+    window.addEventListener("unload", this.unLoadEvent);
+  },
+  beforeUnmount() {
+    window.removeEventListener("beforeunload", this.unLoadEvent);
+    window.addEventListener("unload", this.unLoadEvent);
+  },
+  methods: {
+    unLoadEvent: function (event) {
+      // if (this.isLeaveSite) return;
+      if (!this.$store.getters.isUserStayLogin) {
+        localStorage.removeItem("refreshToken");
+        deleteCookie("accessToken");
+        localStorage.removeItem("oauthInfo");
+      }
+      this.$store.event.preventDefault();
+      event.returnValue = "";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
