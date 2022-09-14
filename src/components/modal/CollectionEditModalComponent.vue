2600566a014eb9ad6de2a77dc384614a861bd707
oseunghyun <ohsh1477@gmail.com>
2022-09-14 20:50:49 +0900

    [fix] 미분류 카테고리로 수정 시에 아무 값 안가도록 임시로 처리

--- src/components/modal/CollectionEditModalComponent.vue
+++ src/components/modal/CollectionEditModalComponent.vue
@@ -20,23 +20,9 @@
           </div>
           <div class="register-form__wrapper category__wrapper">
             <label class="register-form__label">카테고리</label>
-            <!-- 미분류 카테고리인 경우 -->
-            <select
-              v-if="collections.category == null"
-              v-model="categoryName"
-              class="contents-modal__select"
-            >
-              <option value="">미분류</option>
-              <option v-for="(category, index) in myCategories" :key="index">
-                {{ category.name }}
-              </option>
-            </select>
+
             <!-- 미분류 카테고리 아닌 경우 -->
-            <select
-              v-if="collections.category"
-              v-model="collections.category.name"
-              class="contents-modal__select"
-            >
+            <select v-model="categoryName" class="contents-modal__select">
               <option value="">미분류</option>
               <option v-for="(category, index) in myCategories" :key="index">
                 {{ category.name }}
@@ -188,6 +174,11 @@ export default {
       (data) => data.id == this.collectionId
     );
     this.collections = this.collections[0];
+    if (this.collections.category == null) {
+      this.categoryName = "";
+    } else if (this.collections.category !== null) {
+      this.categoryName = this.collections.category.name;
+    }
   },
   methods: {
     addFavorites() {
