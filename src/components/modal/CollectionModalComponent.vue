89423039f36cbfc439f24e0ff8002dce1aac8337
oseunghyun <ohsh1477@gmail.com>
2022-11-18 23:21:51 +0900

    fix(collectionModal): 링크 2개 콜렉션저장 시 1번링크 사라짐 이슈 수정 - #165

--- src/components/modal/CollectionModalComponent.vue
+++ src/components/modal/CollectionModalComponent.vue
@@ -107,14 +107,14 @@
 </template>
 
 <script>
-import closeBtn from "@/assets/icon/closeBtn.svg";
-import star_border from "@/assets/icon/star_border.svg";
-import star from "@/assets/icon/star.svg";
-import alert_circle from "@/assets/icon/alert-circle.svg";
-import add_link from "@/assets/icon/addLink.svg";
-import minus from "@/assets/icon/minus.svg";
-import AlertModalComponent from "@/components/modal/AlertModalComponent.vue";
-import { addCollection } from "@/api/collection";
+import closeBtn from '@/assets/icon/closeBtn.svg';
+import star_border from '@/assets/icon/star_border.svg';
+import star from '@/assets/icon/star.svg';
+import alert_circle from '@/assets/icon/alert-circle.svg';
+import add_link from '@/assets/icon/addLink.svg';
+import minus from '@/assets/icon/minus.svg';
+import AlertModalComponent from '@/components/modal/AlertModalComponent.vue';
+import { addCollection } from '@/api/collection';
 
 export default {
   components: { AlertModalComponent },
@@ -132,25 +132,24 @@ export default {
       myCategories: {},
       // 경고 모달
       isAlertModalActive: false,
-      alertModalContent: "",
-      btnMessage: "네",
+      alertModalContent: '',
+      btnMessage: '네',
       // 삭제 경고 모달
       isDeleteModalActive: false,
-      deleteModalContent: "해당 콜렉션을 \n삭제하시겠습니까?",
+      deleteModalContent: '해당 콜렉션을 \n삭제하시겠습니까?',
       // 폼 항목
-      categoryName: "",
+      categoryName: '',
     };
   },
   props: {
     collectionData: {
       type: Object,
-      default: () => ({ contentLinkList: ["1", "2"] }),
+      default: () => ({ contentLinkList: ['1', '2'] }),
     },
   },
   async created() {
-    await this.$store.dispatch("GET_CATEGORIES");
+    await this.$store.dispatch('GET_CATEGORIES');
     this.myCategories = this.$store.getters.getCategories;
-    this.collectionData.contentLinkList[0] = "";
   },
   methods: {
     addFavorites() {
@@ -159,7 +158,7 @@ export default {
 
     // 인풋 추가 이벤트
     createInput() {
-      this.collectionData.contentLinkList.push("");
+      this.collectionData.contentLinkList.push('');
     },
     // 인풋 추가 이벤트
     deleteInput(index) {
@@ -174,28 +173,26 @@ export default {
         contentLinkList: this.collectionData.contentLinkList.filter(function(
           item
         ) {
-          return item !== null && item !== undefined && item !== "";
+          return item !== null && item !== undefined && item !== '';
         }),
         favorite: this.collectionData.favorite,
       };
       Object.keys(collectionData).forEach(
         (key) =>
-          (collectionData[key] == "" || collectionData[key] == undefined) &&
+          (collectionData[key] == '' || collectionData[key] == undefined) &&
           delete collectionData[key]
       );
       try {
-        const response = await addCollection(collectionData);
-        console.log(response);
-        this.$emit("close-modal");
+        await addCollection(collectionData);
+        this.$emit('close-modal');
       } catch (error) {
-        console.log(error);
         this.alertModalContent = error.response.data.message;
         this.isAlertModalActive = true;
       } finally {
-        this.collectionData.title = "";
-        this.collectionData.comment = "";
-        this.collectionData.contentsLinkList = "";
-        this.collectionData.categoryName = "";
+        this.collectionData.title = '';
+        this.collectionData.comment = '';
+        this.collectionData.contentsLinkList = '';
+        this.collectionData.categoryName = '';
         this.collectionData.favorite = false;
       }
     },
