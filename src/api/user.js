2d869c09d6c348c99869ecc4557608f1eb94ef2a
oseunghyun <ohsh1477@gmail.com>
2023-02-24 12:02:46 +0900

    fix(api): 유저 콘텐츠 조회 api를 contents로 이동 - #204

--- src/api/user.js
+++ src/api/user.js
@@ -15,14 +15,4 @@ function getProfile() {
   return authInstance.get('users/me');
 }
 
-// 자신의 콘텐츠 조회
-function getMyContents(categoryId) {
-  return authInstance.get(`users/load-contents?categoryId=${categoryId}`);
-}
-
-// 자신의 즐겨찾기 조회
-function getMyFavorites() {
-  return authInstance.get('users/load-favorites');
-}
-
-export { editProfile, resetPw, getProfile, getMyContents, getMyFavorites };
+export { editProfile, resetPw, getProfile };
